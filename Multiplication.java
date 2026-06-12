9c9d8bd65cdea91c68211745e0f227c35e8168f4
Abhishek Sharma <abhisru97@gmail.com>
2026-06-12 12:52:07 +0530

    Multiplication and Division programs in Java

---
+++ Multiplication.java
@@ -0,0 +1,8 @@
+public class Multiplication {
+    public static void main (String[] args)
+{
+    int a=100,b= 10;
+    int mul = a*b;
+    System.out.println("The mul of a & b is : " + mul);
+}
+}
---
+++ division.java
@@ -0,0 +1,9 @@
+public class division {
+    public static void main(String[] args)
+    {
+        float a = 100,b= 30;
+        float div= a/b;
+        System.out.println("The div of a and b is :"+div);
+        
+    }
+}
