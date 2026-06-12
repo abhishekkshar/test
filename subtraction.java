57b616944c06c73c3ad5bdcab42f05f2e4109106
Abhishek Sharma <abhisru97@gmail.com>
2026-06-12 11:52:31 +0530

    Addition & subtraction programs in java.

---
+++ subtraction.java
@@ -0,0 +1,8 @@
+public class subtraction {
+    public static void main(String[] args)
+    {
+        int a= 50, b= 10;
+        int sub = a - b;
+        System.out.println("The sub of a and b is :" + sub);
+    }
+}
